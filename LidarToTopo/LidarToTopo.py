7d93c2fea09c6295c50be0cff11998163a708415
Jacob Seamus Olson <jacobrolson14@gmail.com>
2024-09-25 20:09:16 -0700

    Set up tool environment for LidarToTopo
    Set up the environment for the LidarToTopo tool as an example

--- LidarToTopo/LidarToTopo.py
+++ LidarToTopo/LidarToTopo.py
@@ -3,16 +3,16 @@ import os
 
 def lidar_process(in_lidar, final_out):
     # Place lidar points in correct geographic coordinates with XY table to point
-    xy_result = r"G:\Tools\LidarToTopo\temp_data.gdb\xy_result"
+    xy_result = r"C:\GitHub\arcpy-scripts\LidarToTopo\temp_data.gdb\xy_result"
     arcpy.management.XYTableToPoint(in_table=in_lidar, out_feature_class=xy_result, x_field="longitude", y_field="latitude")
 
     # Project lidar points to correct state plane projection
-    proj_result = r"G:\Tools\LidarToTopo\temp_data.gdb\proj_result"
+    proj_result = r"C:\GitHub\arcpy-scripts\LidarToTopo\temp_data.gdb\proj_result"
     sr = sp_project(in_points=xy_result, out_proj=proj_result)
     arcpy.management.Delete(xy_result)
 
     # Create a TIN with projected lidar
-    tin_result = r"G:\Tools\LidarToTopo\temp_data\tin_result"
+    tin_result = r"C:\GitHub\arcpy-scripts\LidarToTopo\temp_data\tin_result"
     arcpy.ddd.CreateTin(out_tin=tin_result, spatial_reference=sr, in_features=f"{proj_result} Z")
     arcpy.management.Delete(proj_result)
 
@@ -21,12 +21,12 @@ def lidar_process(in_lidar, final_out):
     arcpy.ddd.DelineateTinDataArea(in_tin=tin_result, max_edge_length=max_edge)
 
     # Create raster from TIN
-    ras_result = r"G:\Tools\LidarToTopo\temp_data.gdb\ras_result"
+    ras_result = r"C:\GitHub\arcpy-scripts\LidarToTopo\temp_data.gdb\ras_result"
     arcpy.ddd.TinRaster(in_tin=tin_result, out_raster=ras_result, sample_distance="CELLSIZE", sample_value=1)
     arcpy.management.Delete(tin_result)
 
     # Create contour lines from raster
-    con_result = r"G:\Tools\LidarToTopo\temp_data.gdb\con_result"
+    con_result = r"C:\GitHub\arcpy-scripts\LidarToTopo\temp_data.gdb\con_result"
     arcpy.sa.Contour(in_raster=ras_result, out_polyline_features=con_result, contour_interval=10)
     arcpy.management.Delete(ras_result)
 
@@ -37,10 +37,10 @@ def lidar_process(in_lidar, final_out):
     return
 
 def sp_project(in_points, out_proj):
-    state_plane = r"G:\Tools\LidarToTopo\temp_data.gdb\CA_State_Plane_Zones"
+    state_plane = r"C:\GitHub\arcpy-scripts\LidarToTopo\temp_data.gdb\CA_State_Plane_Zones"
 
     # Perform summarize within tool to find number of lidar points in each state plane
-    sum_within = r"G:\Tools\LidarToTopo\temp_data.gdb\sum_within"
+    sum_within = r"C:\GitHub\arcpy-scripts\LidarToTopo\temp_data.gdb\sum_within"
     arcpy.analysis.SummarizeWithin(in_polygons=state_plane, in_sum_features=in_points, out_feature_class=sum_within)
 
     # Get state plane zone with highest number of lidar points
@@ -66,8 +66,8 @@ def sp_project(in_points, out_proj):
 
 def get_ps(lidar_points):
     # Run LAStools shp2las in OS system
-    las_result = r"G:\Tools\LidarToTopo\temp_data\las_result.las"
-    las_com = fr"G:\Tools\LAStools\bin\shp2las.exe -i {lidar_points} -o {las_result}"
+    las_result = r"C:\GitHub\arcpy-scripts\LidarToTopo\temp_data\las_result.las"
+    las_com = fr"C:\GitHub\arcpy-scripts\LidarToTopo\LAStools\bin\shp2las.exe -i {lidar_points} -o {las_result}"
     os.system(las_com)
 
     # Run LAS dataset statistics to return point spacing
